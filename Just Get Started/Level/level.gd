f482e4fb52618d6b1db34fb7aae8695ef68824c5
Nikmorking <nikmor.2010.king@gmail.com>
2025-07-15 13:51:17 +0300

    смерь и ООП

---
+++ Just Get Started/Level/level.gd
@@ -0,0 +1,38 @@
+extends Node2D
+class_name Level
+
+var door_action = false
+var start_pos
+
+var door_x = 717
+var door_y = 34
+
+func _process(delta):
+	pass
+
+func door_state(state):
+	if state == false:
+		$closeDoor.visible = true
+		$openDoor.visible = false
+	else:
+		$closeDoor.visible = false
+		$openDoor.visible = true
+	pass
+
+
+
+func _on_area_2d_body_entered(body):
+	door_action = true
+	door_state(true)
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	door_action = false
+	door_state(false)
+	pass # Replace with function body.
+
+func _on_out(body):
+	$Player.position = start_pos
+	$Player/AnimatedSprite2D.flip_h = false
+	pass # Replace with function body.d
--- Just Get Started/Level/level2.gd
+++ Just Get Started/Level/level2.gd
@@ -1,39 +1,12 @@
-extends Node2D
-
-var door_action = false
+extends Level
 
 func _ready():
-	pass 
-
-var door_x = 717
-var door_y = 34
-
-func _process(delta):
-	pass
-
-func door_state(state):
-	if state == false:
-		$closeDoor.visible = true
-		$openDoor.visible = false
-	else:
-		$closeDoor.visible = false
-		$openDoor.visible = true
+	door_x = 717
+	door_y = 34
+	start_pos = $Player.position
 	pass
 
 func _input(event):
 	if Input.is_action_just_pressed("e") && door_action:
 		get_tree().change_scene_to_file("res://menu/Menu.tscn")
 	pass
-
-
-
-func _on_area_2d_body_entered(body):
-	door_action = true
-	door_state(true)
-	pass # Replace with function body.
-
-
-func _on_area_2d_body_exited(body):
-	door_action = false
-	door_state(false)
-	pass # Replace with function body.
