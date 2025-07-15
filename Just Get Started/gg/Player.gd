1ab03415ff3ebd6d4cf2f55e1a1dd95f6ea3caed
matildazz <raeiei725@gmail.com>
2025-07-15 21:39:19 +0300

    dash

--- Just Get Started/gg/Player.gd
+++ Just Get Started/gg/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var SPEED = 250.0
 @export var JUMP_VELOCITY = -350.0
 var dashKd: bool = true
-var dashBlock: bool = false
+var timerBlock: bool = false
 var direction
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -11,7 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_pressed("dash")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	direction = Input.get_axis("ui_left", "ui_right")
 	pass
@@ -39,12 +39,12 @@ func _on_timer_2_timeout():
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor() and dashKd:
+	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
 	
-	if direction and dashKd:
+	if direction:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.play("run")
 		if direction == 1:
