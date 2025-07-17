a74a9e7d3cc78d3df9067dd0d0c9e7fd777d7759
Nikmorking <nikmor.2010.king@gmail.com>
2025-07-17 10:29:42 +0300

    Reapply "Merge branch 'nikmor' of https://github.com/Nikmorking/Just-get-start into nikmor"
    This reverts commit 221b9246d549b559287e9606dfa99560589c3ba1.

--- Just Get Started/Level/level2.gd
+++ Just Get Started/Level/level2.gd
@@ -9,11 +9,6 @@ func _ready():
 func _input(event):
 	if Input.is_action_just_pressed("e") && door_action:
 		get_tree().change_scene_to_file("res://Level/level3.tscn")
+		Global.level = 3
+	Global.save_game()
 	pass
-
-
-
-func _on_lock(body):
-	$Camera2D/AnimationPlayer.play("lock")
-	$Player.JUMP_VELOCITY = -500
-	pass # Replace with function body.
